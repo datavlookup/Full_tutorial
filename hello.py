3bfd7de410c0f2b47538b615d2a66a11d563ee4c
Orhan <ahmedovorhan47@gmail.com>
2023-11-20 16:23:51 +0100

    Updated version

--- hello.py
+++ hello.py
@@ -1,14 +1,38 @@
 from flask import *
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
 
 
 app = Flask(__name__, template_folder='templates')
+# Create Key 
+app.config['SECRET_KEY'] = 'MY_SECRET_KEY_GOOD'
+
+# CREATE CLASS FORM
+class NamerForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+    
+# Create Name Page 
+@app.route('/name', methods=['POST', 'GET'])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        
+    return render_template('name.html', 
+                           name=name,
+                           form=form)
+
 
 @app.route('/')
 def index():
     first_name = 'Orhan'
     stuff = 'This is <strong> bold </strong> '
     favorite_pizza = ['paperroni', 'mushrooms', 'Cheese', 'pizza', 'Salami']
-        
+
     return render_template('index.html', first_name=first_name, stuff=stuff, favorite_pizza=favorite_pizza)
 
 @app.route('/user/<name>')
